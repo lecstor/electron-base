172d3e7b4436b1247014313352df9fe77e3ab40a
Jason Galea <jason@lecstor.com>
2020-08-05 21:38:53 +1000

    add Chakra-UI

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,6 +1,5 @@
-import { hot } from "react-hot-loader";
 import React, { FC } from "react";
 
-const App: FC = () => <div>Hi from react!</div>;
+const Root: FC = () => <div>Hi from react!</div>;
 
-export default hot(module)(App);
+export default Root;
